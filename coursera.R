73296ee8f7855c674c78d7ae0b607146219162f8
Aessam8080 <91079508+Aessam8080@users.noreply.github.com>
2021-09-25 17:50:25 +0200

    Add files via upload

---
+++ coursera.R
@@ -0,0 +1,46 @@
+
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL                         #initializing inverse as null
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x                #function to get matrix x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {        #gets cache data
+  inv <- x$getinverse()
+  if(!is.null(inv)) {                #check if inverse is null
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)                 #get inverse of matrix
+  inv
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
